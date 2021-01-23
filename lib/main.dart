7b88a5c4746577a7e316150fc8a9cf0248982f36
jaylad29 <jay.lad3494@gmail.com>
2021-01-23 16:14:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:analog_clock/model/my_theme_provider.dart';
+import 'package:analog_clock/screens/home_screens.dart';
+import 'package:analog_clock/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => MyThemeModel(),
+        child: Consumer<MyThemeModel>(
+          builder: (context, theme, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Analog Clock',
+            theme: themeData(context),
+            darkTheme: darkThemeData(context),
+            themeMode: theme.isLightTheme ? ThemeMode.light : ThemeMode.dark,
+            home: HomeScreen(),
+          ),
+        ));
+  }
+}
